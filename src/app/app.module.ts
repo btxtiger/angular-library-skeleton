a4f13f4a187cf30488c49f0d6320ab7c354c9784
Jonas <btxtiger@icloud.com>
2023-10-07 22:37:14 +0200

    Test override colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxLibComponent } from '../../projects/ngx-lib/src/lib/ngx-lib.component';
-// import { NgxLibComponent } from 'ngx-lib';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Import the library
+// import { NgxLibComponent } from '../../projects/ngx-lib/src/lib/ngx-lib.component';
+import { NgxLibComponent } from 'ngx-lib';
 
 @NgModule({
    declarations: [AppComponent],
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
    content: ['./src/**/*.{html,ts}', './projects/ngx-lib/src/**/*.{html,ts}'],
    darkMode: 'class',
    theme: {
-      extend: {},
+      extend: {
+         // Override colors test
+         // colors: {
+         //    red: {
+         //       50: '#fdf8f6',
+         //       100: '#f2e8e5',
+         //       200: '#eaddd7',
+         //       300: '#e0cec7',
+         //       400: '#d2bab0',
+         //       500: '#bfa094',
+         //       600: '#a18072',
+         //       700: '#977669',
+         //       800: '#846358',
+         //       900: '#43302b',
+         //    },
+         // },
+      },
    },
    plugins: [],
    experimental: {
